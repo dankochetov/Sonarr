9ac1a515babbc6d9d798332a837883b259af04ca
Mark McDowall <mark@mcdowall.ca>
2025-12-07 18:17:46 -0800

    New: OIDC authentication
    Closes #2477

---
+++ src/NzbDrone.Core/Authentication/AuthenticationConfigurationExtensions.cs
@@ -0,0 +1,44 @@
+using System;
+using System.Linq;
+using NzbDrone.Common.Extensions;
+using NzbDrone.Core.Configuration;
+
+namespace NzbDrone.Core.Authentication
+{
+    public static class AuthenticationConfigurationExtensions
+    {
+        public static bool IsOidcConfigured(this IConfigFileProvider configFileProvider)
+        {
+            return configFileProvider.HasOidcSettings() && configFileProvider.IsOidcAuthoritySecure();
+        }
+
+        public static bool HasOidcSettings(this IConfigFileProvider configFileProvider)
+        {
+            return configFileProvider.OidcAuthority.IsNotNullOrWhiteSpace() &&
+                   configFileProvider.OidcClientId.IsNotNullOrWhiteSpace() &&
+                   configFileProvider.OidcClientSecret.IsNotNullOrWhiteSpace() &&
+                   configFileProvider.OidcUserIdentifier.IsNotNullOrWhiteSpace() &&
+                   GetOidcScopes(configFileProvider.OidcScopes).Contains("openid");
+        }
+
+        public static bool IsOidcAuthoritySecure(this IConfigFileProvider configFileProvider)
+        {
+            return configFileProvider.OidcAuthority.IsNotNullOrWhiteSpace() &&
+                   configFileProvider.OidcAuthority.StartsWith("https://", StringComparison.OrdinalIgnoreCase);
+        }
+
+        public static string[] GetOidcScopes(string scopes)
+        {
+            return scopes?.Split(new[] { ' ', ',' }, StringSplitOptions.RemoveEmptyEntries | StringSplitOptions.TrimEntries) ?? Array.Empty<string>();
+        }
+
+        public static AuthenticationType EffectiveAuthenticationMethod(this IConfigFileProvider configFileProvider)
+        {
+            var authenticationMethod = configFileProvider.AuthenticationMethod;
+
+            return authenticationMethod == AuthenticationType.Oidc && !configFileProvider.IsOidcConfigured()
+                ? AuthenticationType.Forms
+                : authenticationMethod;
+        }
+    }
+}
